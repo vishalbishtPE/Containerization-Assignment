4e244b0712f9bd9c03e689a231901bb1c1fb5cdf
vishalbishtPE <51356233+vishalbishtPE@users.noreply.github.com>
2019-07-04 16:30:02 +0530

    Add files via upload

---
+++ Q2A/app.py
@@ -0,0 +1,26 @@
+import time, redis
+from flask import Flask
+
+#Create an instance of Flask and Redis
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+#Logic for finding number of visits
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
